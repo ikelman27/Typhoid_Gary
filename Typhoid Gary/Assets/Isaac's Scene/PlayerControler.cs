dec185055c36d90f94043b963cecf84ea90891f4
Isaac Kelman <ikelman27@gmail.com>
2018-01-27 01:19:46 -0500

    added cough and sneeze

--- Typhoid Gary/Assets/Isaac's Scene/PlayerControler.cs
+++ Typhoid Gary/Assets/Isaac's Scene/PlayerControler.cs
@@ -2,95 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour {
+public class PlayerControler : MonoBehaviour
+{
 
     #region variables
     private float speed;
-    private Vector2 position;
+    private Vector3 position;
     private GameObject player;
-    private float rotation;
-
+    Vector2 cameraPos;
+    Vector2 mousePos;
+    float angle;
+    public GameObject snot;
+    Vector3 snotDir;
+    public float snezeCooldownTime;
+    float snezeTimeStamp;
     #endregion
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         player = GameObject.Find("Player");
         position = player.transform.position;
-       
-	}
-	
-    
-	// Update is called once per frame
-	void Update () {
+        snot.SetActive(false);
+        snezeTimeStamp = snezeCooldownTime + Time.time;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (Input.GetKey("a"))
         {
             position.x -= .1f;
-            rotation = 270;
+
         }
 
         if (Input.GetKey("d"))
         {
             position.x += .1f;
-            rotation = 90;
+
         }
 
         if (Input.GetKey("w"))
         {
-            position.y += .1f;
-           
-
-            if (Input.GetKey("a"))
-            {
-                rotation = 315;
-
-            }
-            else if (Input.GetKey("d"))
-            {
-                rotation  = 45;
-            }
-            else
-            {
-                rotation = 0;
-            }
+            position.z += .1f;
 
         }
-
-        
-
         if (Input.GetKey("s"))
         {
-            position.y -= .1f;
+            position.z -= .1f;
 
-
-            if (Input.GetKey("a"))
-            {
-                rotation = 225;
-
-            }
-            else if (Input.GetKey("d"))
-            {
-                rotation =  135;
-            }
-            else
-            {
-                rotation = 180;
-            }
         }
 
-        
 
-        
+        cameraPos = Camera.main.WorldToViewportPoint(transform.position);
+        mousePos = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        angle = Mathf.Atan2(cameraPos.y - mousePos.y, cameraPos.x - mousePos.x) * Mathf.Rad2Deg;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            sneeze();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            cough();
+        }
 
 
         movePlayer();
-	}
+    }
+
 
     void movePlayer()
     {
-        player.transform.rotation = Quaternion.Euler(player.transform.rotation.x, player.transform.rotation.y, rotation);
+        player.transform.rotation = Quaternion.Euler(new Vector3(0f, -angle, 0f));
         player.transform.position = position;
-       
+
+        if (snot.activeInHierarchy)
+        {
+            snot.transform.position += snotDir / 10;
+        }
+
+    }
+
+
+
+    void sneeze()
+    {
+        if (snezeTimeStamp <= Time.time)
+        {
+            snot.transform.position = player.transform.position;
+            snot.SetActive(true);
+            snotDir = -player.transform.right;
+            snezeTimeStamp = snezeCooldownTime + Time.time;
+        }
+    }
+
+    void cough()
+    {
+
     }
 }
