58430bc10bfe78f1c4546454b7e1b50b47851f70
Samantha Levey (RIT Student) <sxl9302@ad.rit.edu>
2018-01-26 22:22:40 -0500

    Started on AI

---
+++ Typhoid Gary/Assets/S_L_Stuff/SensoryScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensoryScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] sensors = GameObject.FindGameObjectsWithTag("Sensor");
+        foreach(GameObject s in sensors)
+        {
+            s.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
