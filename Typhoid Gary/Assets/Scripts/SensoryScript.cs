f30dee135ab5ab53964447f8e9b3714943699239
Samantha Levey (RIT Student) <sxl9302@ad.rit.edu>
2018-01-27 13:24:24 -0500

    Pushing working sensory system

--- Typhoid Gary/Assets/Scripts/SensoryScript.cs
+++ Typhoid Gary/Assets/Scripts/SensoryScript.cs
@@ -4,114 +4,53 @@ using UnityEngine;
 
 public class SensoryScript : MonoBehaviour {
 
-    // Movement
-    Vector3 acceleration;
-    Vector3 velocity;
-    Vector3 desired;
-    Vector3 force;
-
-    // public to change in Inspector
-    public float maxSpeed = 10.0f;
-    public float maxForce = 12.0f;
-    public float mass = 2.0f;
-    public float radius = 1.0f;
-
+    public bool isSight;
+    public bool isHearing;
 
     bool hearing = false;
+    bool seeing = false;
 
     GameObject target;
 
-    public string tag;
+    public string findTag;
 
     // Use this for initialization
     void Start () {
+        // Turns everything invisible upon startup
         GameObject[] sensors = GameObject.FindGameObjectsWithTag("Sensor");
         foreach(GameObject s in sensors)
         {
             s.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
         }
-        target = GameObject.FindGameObjectWithTag(tag);
+
+        // Sets target to findTag's tag
+        target = GameObject.FindGameObjectWithTag(findTag);
 	}
 	
-    bool Hearing
+    public bool Hearing
     {
         get { return hearing; }
         set { hearing = value; }
     }
 
+    public bool Seeing
+    {
+        get { return seeing; }
+        set { seeing = value; }
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
-
-
-        // ------------------------------------------------------------- >> STEERING <<
-        // Calculate the necessary steering forces / add them to acceleration
-        CalcSteeringForces();
-
-        // For clamping the object down to the ground
-        Vector3 down = new Vector3(0f, -1f, 0f);
-        down *= maxSpeed;
-
-        // Add accel to vel
-        velocity += acceleration * Time.deltaTime;
-        velocity.y = 0;
-
-        // Limit vel to max speed
-        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
-
-        // Move character based on velocity - don't forget to clamp to ground
-        velocity.y = 0.0f;
-        transform.position += velocity * Time.deltaTime;
-        //charController.Move(velocity * Time.deltaTime);
-        //charController.Move(down * Time.deltaTime);
-
-        // Reset acceleration to 0
-        acceleration = Vector3.zero;
-
-        // Set creature's forward to the velocity now
-        transform.forward = velocity.normalized;
-
-    }
-
-
-    // Calculates all steering forces / adds them together into acceleration
-    // Calculates depending on SUBSUMPTION model
-    void CalcSteeringForces()
-    {
-        // Reset forces
-        force = Vector3.zero;
-
-        force += Seek(target.transform.position);
-
-
-        // Clamp the force and apply it
-        force = Vector3.ClampMagnitude(force, maxForce);
-
-        ApplyForce(force);
-    }
-
-    // Applies force to acceleration, taking mass into account
-    void ApplyForce(Vector3 steeringForce)
-    {
-        acceleration += steeringForce / mass;
-    }
-
-    // Returns direction creature must travel to reach target
-    Vector3 Seek(Vector3 targetPos)
-    {
-        desired = targetPos - transform.position;
-        desired.y = 0;
-        desired = desired.normalized * maxSpeed;
-        desired -= velocity;
-        return desired;
-    }
-
-    Vector3 Arrive(Vector3 targetPos)
-    {
-        if(Vector3.Distance(transform.position, targetPos) < 2.0f)
+        if (hearing)
         {
-
+            Debug.Log("HEARING");
+            // To add: code about raising suspicion
         }
-        return Vector3.zero;
+        else if (seeing)
+        {
+            Debug.Log("SEEING");
+        }
+
     }
 }
