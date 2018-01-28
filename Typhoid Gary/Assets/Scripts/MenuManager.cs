0c47e0406db7e9837a79eb9760ace3a4a01d9af0
SulfuricSulfur <rc7604@g.rit.edu>
2018-01-27 20:46:30 -0500

    MainMenu
    Added the main menu and added in script to make title float

---
+++ Typhoid Gary/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MenuManager : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject credits;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClickCreds()
+    {
+        credits.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+    public void back()
+    {
+        mainMenu.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
